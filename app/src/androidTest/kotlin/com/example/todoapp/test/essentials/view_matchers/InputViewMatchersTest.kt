9c8405329f37fd6d6f66b6e9ac2d6957bb4d2eb2
Yehor Yehorychev <egor57899@gmail.com>
2024-12-11 17:55:31 -0800

    clickViewActionsClick test has been refactored

---
+++ app/src/androidTest/kotlin/com/example/todoapp/test/essentials/view_matchers/InputViewMatchersTest.kt
@@ -0,0 +1,36 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.todoapp.test.essentials.view_matchers
+
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.hasImeAction
+import androidx.test.espresso.matcher.ViewMatchers.supportsInputMethods
+import com.example.todoapp.test.essentials.BaseTest
+import org.junit.Test
+
+/**
+ * Demonstrates Espresso Input view matchers.
+ */
+class InputViewMatchersTest : BaseTest() {
+
+    /**
+     * [ViewMatchers.supportsInputMethods] matches a [View] with supported input methods.
+     */
+    @Test
+    fun inputSupportsInputMethods() {
+        onView(supportsInputMethods())
+    }
+
+    /**
+     * [ViewMatchers.hasImeAction] matches a [View] with supported input methods and specified IME action.
+     */
+    @Test
+    fun inputHasImeAction() {
+        onView(hasImeAction(EditorInfo.IME_ACTION_NEXT))
+        onView(hasImeAction(EditorInfo.IME_ACTION_SEARCH))
+        onView(hasImeAction(EditorInfo.IME_ACTION_DONE))
+    }
+}
